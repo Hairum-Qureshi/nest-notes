3e3fcd4d07e06f0959cc7a088a1c0fc2741ec5e9
Hairum Qureshi <hqureshi@udel.edu>
2025-09-15 19:27:11 -0400

    created Landing page and moved code from App component to Landing while setting up page routes in App component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,22 +1,18 @@
 import "../css/app.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./Landing";
+// import About from "./About";
+// import NotFound from "./NotFound";
+
 export default function App() {
 	return (
-		<div className="bg-gradient-to-tr from-black via-black to-sky-700 min-h-screen max-h-auto">
-			<div className="p-10">
-				<h1
-					className="text-white text-6xl
-                  [text-shadow:0_0_8px_rgba(56,189,248,0.8),0_0_16px_rgba(56,189,248,0.6),0_0_24px_rgba(56,189,248,0.4)]"
-				>
-					Nest Notes
-				</h1>
-				<h2
-					className="text-white text-4xl my-3
-                  [text-shadow:0_0_8px_rgba(56,189,248,0.8),0_0_16px_rgba(56,189,248,0.6),0_0_24px_rgba(56,189,248,0.4)]"
-				>
-					Hello World
-				</h2>
-			</div>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				{/* <Route path="/About" element={<About />} />
+					<Route path="*" element={<NotFound />} /> */}
+			</Routes>
+		</BrowserRouter>
 	);
 }
---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,20 @@
+export default function Landing() {
+	return (
+		<div className="bg-gradient-to-tr from-black via-black to-sky-700 min-h-screen max-h-auto">
+			<div className="p-10">
+				<h1
+					className="text-white text-6xl
+                  [text-shadow:0_0_8px_rgba(56,189,248,0.8),0_0_16px_rgba(56,189,248,0.6),0_0_24px_rgba(56,189,248,0.4)]"
+				>
+					Nest Notes
+				</h1>
+				<h2
+					className="text-white text-4xl my-3
+                  [text-shadow:0_0_8px_rgba(56,189,248,0.8),0_0_16px_rgba(56,189,248,0.6),0_0_24px_rgba(56,189,248,0.4)]"
+				>
+					Hello World
+				</h2>
+			</div>
+		</div>
+	);
+}
